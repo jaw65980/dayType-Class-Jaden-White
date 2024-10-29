75f0e0d5a4d8908e1a591db8febe606ddfe990e3
Jaden White <jaw65980@email.vccs.edu>
2024-10-28 22:32:53 -0400

    Added comments

--- dayType Class Jaden White/dayType Class Jaden White.cpp
+++ dayType Class Jaden White/dayType Class Jaden White.cpp
@@ -3,10 +3,13 @@
 #include <iomanip>
 using namespace std;
 
+//All the functions and variables are held in the dayType class.
 class dayType
 {
 public:
-    void test()
+    //This function both prints the day of the week and allows users to make an input that will call another function depending on what they type. 
+    // Do note these responses are case sensitive.
+    void inputprompt()
     {
         cout << "The day is " << daysOfweek[dayset] << ". What would you like to do?" << endl;
         cout << "Type Next to go to the next day." << endl;
@@ -36,10 +39,13 @@ public:
         }
     }
 private:
+    //These are the main variables the program uses in its functions.
     string daysOfweek[7] = { "Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday" };
     int dayset = 0;
     string response;
 
+    //This function will add an index to the table which allows it to display the next day.
+    //If the index goes to 6, the index will drop to -1 so the next index can be 0. This turns the day from Saturday to Sunday.
     void nextDay()
     {
         if (dayset >= 6)
@@ -48,9 +54,11 @@ private:
         }
         dayset++;
         cout << endl;
-        test();
+        inputprompt();
     }
 
+    //This function will subtract an index from the table which allows it to display the previous day.
+    //Like the nextDay function, if the index goes under 0, the index will rise to 7. This will turn Sunday to Saturday.
     void previousDay()
     {
         if (dayset <= 0)
@@ -59,9 +67,11 @@ private:
         }
         dayset--;
         cout << endl;
-        test();
+        inputprompt();
     }
 
+    //This function asks the user what day of the week they'd like to set. 
+    //Depending on the answer, the index will change to a specific number which correlates with the desired day of the week.
     void setDay()
     {
         cout << endl << "What would you like to set the day to?" << endl;
@@ -103,9 +113,13 @@ private:
             dayset = 6;
         }
         cout << endl;
-        test();
+        inputprompt();
     }
 
+    //This function allows you to add any given number of days and find what day of the week it will land on.
+    //The program will add to the dayset index depending on the input the user gives.
+    //If the dayset index is greater than or equal to 7, the index will go back down to 0 to create a loop.
+    //This continues until the number of days the user input has been reached.
     void addDays()
     {
         int addeddays = 0;
@@ -121,12 +135,13 @@ private:
             }
         }
         cout << endl;
-        test();
+        inputprompt();
     }
 };
 
+//This function calls the class.
 int main()
 {
     dayType Days;
-    Days.test();
+    Days.inputprompt();
 }
